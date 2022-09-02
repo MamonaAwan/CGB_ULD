1deea98127b929230401fc31504a49b40940fed2
Mamona Awan <68283380+MamonaAwan@users.noreply.github.com>
2022-09-02 12:34:01 +0400

    Update test_pretrained_model_script.sh

--- test_pretrained_model_script.sh
+++ test_pretrained_model_script.sh
@@ -2,6 +2,6 @@
 
 # python test.py -f pretrained_models_to_test -e mafl --db MAFL --data_path <path>
 
-# python test.py -f pretrained_models_to_test -e cats --db Cats -t 30 --data_path '<path>
+# python test.py -f pretrained_models_to_test -e cats --db Cats -t 30 --data_path <path>
 
 # python test.py -f pretrained_models_to_test -e ls3d --db LS3D -t 16 --data_path <path>
